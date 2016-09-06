eb64d0bd5d901f3378e4a36e6ec724d2c7db8a46
Robert Gorbul <r.gorbul@itransition.com>
2016-09-06 18:42:34 +0300

    Another table editing implementation (not finished)

--- web/js/app.js
+++ web/js/app.js
@@ -1,7 +1,7 @@
 angular.module('candidatedevtest', ['ngTable'])
 
-.controller('ctrl', function($scope, NgTableParams) {
-  $scope.stock = [
+.controller('ctrl', function($scope, $filter, NgTableParams) {
+  var stock = [
     {name: 'Macbook Pro', qty: 5, price: 1500},
     {name: 'Macbook', qty: 3, price: 999},
     {name: 'Asus Zenbook', qty: 4, price: 1200},
@@ -20,178 +20,34 @@ angular.module('candidatedevtest', ['ngTable'])
     sorting: {
       name: 'asc'
     }
-  }, {dataset: angular.copy($scope.stock)});
+  }, {dataset: stock});
 
-  var originalData = angular.copy($scope.stock);
-  $scope.cancel = cancel;
-  $scope.del = del;
-  $scope.save = save;
+  $scope.temp = {};
 
-  function cancel(row, rowForm) {
-    var originalRow = resetRow(row, rowForm);
-    angular.extend(row, originalRow);
+  $scope.editItem = function(item) {
+    item.$edit = !item.$edit;
+    item.$selected = true;
+    item.$valid = true;
+
+    for (var index in item)
+      $scope.temp[index] = item[index];
+  };
+
+  $scope.saveItem = function(item, form) {
+    for (var index in item)
+      item[index] = $scope.temp[index];
+    item.$edit = false;
+    form.$setPristine();
   }
 
-  function del(row) {
-    _.remove($scope.tableParams.settings().dataset, function(item) {
-      return row === item;
-    });
-    $scope.tableParams.reload().then(function(data) {
-      if (data.length === 0 && $scope.tableParams.total() > 0) {
-        $scope.tableParams.page($scope.tableParams.page() - 1);
-        $scope.tableParams.reload();
-      }
-    });
+  $scope.cancelEditing = function(item, form) {
+    item.$edit = false;
+    for (var index in item)
+      $scope.temp[index] = null;
+    form.$setPristine();
   }
 
-  function resetRow(row, rowForm) {
-    row.isEditing = false;
-    rowForm.$setPristine();
-    $scope.tableTracker.untrack(row);
-    return _.findWhere(originalData, function(r) {
-      return r.id === row.id;
-    });
+  $scope.delItem = function(item) {
+    // To-Do
   }
-
-  function save(row, rowForm) {
-    var originalRow = resetRow(row, rowForm);
-    angular.extend(originalRow, row);
-  }
-})
-.controller('trackedTableController', function($scope, $parse, $attrs, $element) {
-  var tableForm = $element.controller('form');
-  var dirtyCellsByRow = [];
-  var invalidCellsByRow = [];
-
-  init();
-
-  function init() {
-    var setter = $parse($attrs.trackedTable).assign;
-    setter($scope);
-    $scope.$on('$destroy', function() {
-      setter(null);
-    });
-
-    $scope.reset = reset;
-    $scope.isCellDirty = isCellDirty;
-    $scope.setCellDirty = setCellDirty;
-    $scope.setCellInvalid = setCellInvalid;
-    $scope.untrack = untrack;
-
-    function getCellsForRow(row, cellsByRow) {
-      return _.find(cellsByRow, function(entry) {
-        return entry.row === row;
-      })
-    }
-
-    function isCellDirty(row, cell) {
-      var rowCells = getCellsForRow(row, dirtyCellsByRow);
-      return rowCells && rowCells.cells.indexOf(cell) !== 1;
-    }
-
-    function reset() {
-      dirtyCellsByRow = [];
-      invalidCellsByRow = [];
-      setInvalid(false);
-    }
-
-    function setCellDirty(row, cell, isDirty) {
-      setCellStatus(row, cell, isDirty, dirtyCellsByRow);
-    }
-
-    function setCellInvalid(row, cell, isInvalid) {
-      setCellStatus(row, cell, isInvalid, invalidCellsByRow);
-      setInvalid(invalidCellsByRow.length > 0);
-    }
-
-    function setCellStatus(row, cell, value, cellsByRow) {
-      var rowCells = getCellsForRow(row, cellsByRow);
-      if (!rowCells && !value)
-        return;
-
-      if (value) {
-        if (!rowCells) {
-          rowCells = {
-            row: row,
-            cells: []
-          };
-          cellsByRow.push(rowCells);
-        }
-        if (rowCells.cells.indexOf(cell) === -1)
-          rowCells.cells.push(cell);
-      }
-      else {
-        _.remove(rowCells.cells, function(item) {
-          return cell === item;
-        });
-        if (rowCells.cells.length === 0) {
-          _.remove(cellsByRow, function(item) {
-            return rowCells === item;
-          });
-        }
-      }
-    }
-
-    function setInvalid(isInvalid) {
-      $scope.$invalid = isInvalid;
-      $scope.$valid = !isInvalid;
-    }
-
-    function untrack(row) {
-      _.remove(invalidCellsByRow, function(item) {
-        return item.row === row;
-      });
-      _.remove(dirtyCellsByRow, function(item) {
-        return item.row === row;
-      });
-      setInvalid(invalidCellsByRow.length > 0);
-    }
-  }
-})
-.controller('trackedTableRowContoller', function($attrs, $element, $parse, $scope) {
-  var row = $parse($attrs.trackedTableRow)($scope);
-  var rowFormCtrl = $element.controller('form');
-  var trackedTableCtrl = $element.controller('trackedTable');
-
-  $scope.isCellDirty = isCellDirty;
-  $scope.setCellDirty = setCellDirty;
-  $scope.setCellInvalid = setCellInvalid;
-
-  function isCellDirty(cell) {
-    return trackedTableCtrl.isCellDirty(row, cell);
-  }
-
-  function setCellDirty(cell, isDirty) {
-    return trackedTableCtrl.setCellDirty(row, cell, isDirty);
-  }
-
-  function setCellInvalid(cell, isInvalid) {
-    return trackedTableCtrl.setCellInvalid(row, cell, isInvalid);
-  }
-})
-.controller('trackedTableCellController', function($attrs, $element, $scope) {
-  var cellFormCtrl = $element.controller('form');
-  var cellName = cellFormCtrl.$name;
-  var trackedTableRowCtrl = $element.controller('trackedTableRow');
-
-  if (trackedTableRowCtrl.isCellDirty(cellName))
-    cellFormCtrl.$setDirty();
-  else
-    cellFormCtrl.$setPristine();
-
-  $scope.$watch(function() {
-    return cellFormCtrl.$dirty;
-  }, function(newValue, oldValue) {
-    if (newValue === oldValue) return;
-
-    trackedTableRowCtrl.setCellDirty(cellName, newValue);
-  });
-
-  $scope.$watch(function() {
-    return cellFormCtrl.$invalid;
-  }, function(newValue, oldValue) {
-    if (newValue === oldValue) return;
-
-    trackedTableRowCtrl.setCellInvalid(cellName, newValue);
-  });
 });
