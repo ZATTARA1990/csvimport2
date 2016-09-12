a5a60c820bee63c11e61dc8f6a432e205ed4ea00
ZATTARA <s.dubovik@itransition.com>
2016-09-12 20:04:26 +0300

    process testing

--- src/CSVParceBundle/Tests/Controller/ProductControllerTest.php
+++ src/CSVParceBundle/Tests/Controller/ProductControllerTest.php
@@ -6,50 +6,74 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ProductControllerTest extends WebTestCase
 {
-    /*
-    public function testCompleteScenario()
+
+    public function testIndexAction()
     {
-        // Create a new client to browse the application
         $client = static::createClient();
 
-        // Create a new entry in the database
         $crawler = $client->request('GET', '/product/');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /product/");
-        $crawler = $client->click($crawler->selectLink('Create a new entry')->link());
 
-        // Fill in the form and submit it
-        $form = $crawler->selectButton('Create')->form(array(
-            'csvparcebundle_product[field_name]'  => 'Test',
-            // ... other fields to fill
-        ));
-
-        $client->submit($form);
-        $crawler = $client->followRedirect();
-
-        // Check data in the show view
-        $this->assertGreaterThan(0, $crawler->filter('td:contains("Test")')->count(), 'Missing element td:contains("Test")');
-
-        // Edit the entity
-        $crawler = $client->click($crawler->selectLink('Edit')->link());
-
-        $form = $crawler->selectButton('Update')->form(array(
-            'csvparcebundle_product[field_name]'  => 'Foo',
-            // ... other fields to fill
-        ));
-
-        $client->submit($form);
-        $crawler = $client->followRedirect();
-
-        // Check the element contains an attribute with value equals "Foo"
-        $this->assertGreaterThan(0, $crawler->filter('[value="Foo"]')->count(), 'Missing element [value="Foo"]');
-
-        // Delete the entity
-        $client->submit($crawler->selectButton('Delete')->form());
-        $crawler = $client->followRedirect();
-
-        // Check the entity has been delete on the list
-        $this->assertNotRegExp('/Foo/', $client->getResponse()->getContent());
+        $this->assertTrue(
+            $client->getResponse()->headers->contains(
+                'Content-Type',
+                'application/json'
+            ),
+            'the "Content-Type" header is "application/json"' // optional message shown on failure
+        );
+        $this->assertTrue($client->getResponse()->isSuccessful(), '200 OK');
     }
 
-    */
+
+    public function testShowAction()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/product/show/2');
+
+        $this->assertTrue(
+            $client->getResponse()->headers->contains(
+                'Content-Type',
+                'application/json'
+            ),
+            'the "Content-Type" header is "application/json"' // optional message shown on failure
+        );
+        $this->assertTrue($client->getResponse()->isSuccessful(), '200');
+
+//        $this->assertContains(
+//            '{"product":{"id":2,"productName":"testedit","price":10,"stock":12,"description":"description1",
+//            "discontinued":1,"discontinuedDate":null}}',
+//            $client->getResponse()->getContent()
+//        );
+
+    }
+
+    public function testNewAction()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request(
+            'POST',
+            '/product/new',
+            array(),
+            array(),
+            array(
+                'CONTENT_TYPE'          => 'application/json',
+            ),
+            '{"product":{"productName":"testedit","price":10,"stock":12,"description":"description1",
+//            "discontinued":1}}'
+        );
+        $this->assertTrue(
+            $client->getResponse()->headers->contains(
+                'Content-Type',
+                'application/json'
+            ),
+            'the "Content-Type" header is "application/json"' // optional message shown on failure
+        );
+       // $this->assertTrue($client->getResponse()->isRedirect(), '201');
+
+
+    }
+
+
+
 }
