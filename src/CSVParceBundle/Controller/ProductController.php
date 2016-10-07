57181ca793f7ff791f0ec5a933b9f2c01247222e
Sergej Dubovik <s.dubovik@itransition.com>
2016-10-07 15:57:40 +0300

    change multiUpdate MultiDelete, add ApiDoc

--- src/CSVParceBundle/Controller/ProductController.php
+++ src/CSVParceBundle/Controller/ProductController.php
@@ -2,25 +2,21 @@
 
 namespace CSVParceBundle\Controller;
 
+use Symfony\Component\DomCrawler\Form;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use CSVParceBundle\Entity\Product;
 use CSVParceBundle\Form\ProductType;
-
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use FOS\RestBundle\View\View as RView;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
-/**
- * Product controller.
- *
- */
+
 class ProductController extends Controller
 {
-
     /**
      * @Rest\View
      *
@@ -32,7 +28,7 @@ class ProductController extends Controller
      *   },
      *   output = "CSVParceBundle\Entity\Product"
      * )
-     *
+     * @return array
      */
     public function indexAction()
     {
@@ -40,16 +36,18 @@ class ProductController extends Controller
 
         $products = $em->getRepository('CSVParceBundle:Product')->findAll();
 
-
         return array(
             'products' => $products,
         );
 
     }
 
-
     /**
      * Finds and displays a Product entity.
+     *
+     * @param Product $product
+     *
+     * @return Product $product
      *
      * @Rest\View
      *
@@ -58,25 +56,18 @@ class ProductController extends Controller
      *   description = "Return  product by id.",
      *   statusCodes = {
      *     200 = "Returned when successful",
+     *     404 = "Returned when product not founded",
      *   },
      *   output = "CSVParceBundle\Entity\Product"
      * )
      */
-    public function showAction($id)
+    public function showAction(Product $product)
     {
-        $em = $this->getDoctrine()->getManager();
-
-        $product = $em->getRepository('CSVParceBundle:Product')->find($id);
-
-        if (!$product) {
-            throw new NotFoundHttpException('Unable to find product.');
-        }
-        return array('product' => $product);
+        return $product;
     }
 
-
     /**
-     * Creates a new Product entity.
+     * Create a new Product entity.
      *
      * @ApiDoc(
      *   resource = true,
@@ -85,7 +76,6 @@ class ProductController extends Controller
      *     201 = "Returned when successful",
      *   },
      *   input ="CSVParceBundle\Form\ProductType" ,
-     *   output = "View"
      * )
      *
      */
@@ -95,17 +85,17 @@ class ProductController extends Controller
 
     }
 
-
     /**
-     * Displays a form to edit an existing Product entity.
+     * Edit an existing Product entity.
      *
      * @ApiDoc(
      *
-     *   description = "Delete product by id",
+     *   description = "Edit product by id",
      *   statusCodes = {
      *     204 = "Returned when successful",
+     *     404 = "Returned when product not founded",
      *   },
-     *   input ="CSVParceBundle\Entity\Product"
+     *   input ="CSVParceBundle\Form\ProductType"
      * )
      *
      */
@@ -115,55 +105,85 @@ class ProductController extends Controller
     }
 
     /**
+     *
+     * Delete product entity by id
+     *
      * @Rest\View(statusCode=204)
+     *
+     * @param Product $product
      *
      * @ApiDoc(
      *
      *   description = "Delete product by id",
      *   statusCodes = {
      *     204 = "Returned when successful",
+     *     404 = "Returned when product not founded",
      *   },
-     *   input ="CSVParceBundle\Entity\Product"
      * )
      */
-
     public function deleteAction(Product $product)
     {
         $em = $this->getDoctrine()->getManager();
         $em->remove($product);
         $em->flush();
-
     }
 
     /**
+     * Delete mane entities from array
+     *
      * @Rest\View(statusCode=204)
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   description = "Input json format [id,id,id,...]. Delete product by id.",
+     *   statusCodes = {
+     *     204 = "Returned when successful",
+     *     404 = "Returned when product not founded",
+     *   },
+     * )
      */
-
     public function allDeleteAction()
     {
-
         $content = $this->getRequest()->getContent();
 
         $products = json_decode($content);
         $em = $this->getDoctrine()->getManager();
 
         foreach ($products as $product_id) {
-            $product = $em->getRepository('CSVParceBundle:Product')->findOneById($product_id);
 
+            $product = $em->getRepository('CSVParceBundle:Product')->findOneById($product_id);
             $em->remove($product);
         }
-
         $em->flush();
-
     }
 
     /**
-     * Displays a form to edit an existing Product entities.
+     * Edit an existing Product entity from array.
+     *
+     * @Rest\View(statusCode=204)
+     *
+     * @ApiDoc(
+     *   resource = true,
+     *   description = "input format
+     *     {products: [
+     * {
+     * id: ,
+     * product_name: ,
+     * price: ,
+     * stock: ,
+     * description: ,
+     * discontinued:
+     * }
+     * ]}",
+     *   statusCodes = {
+     *     204 = "Returned when successful",
+     *     404 = "Returned when product not founded",
+     *   },
+     * )
      *
      */
     public function multiEditAction()
     {
-
         $serialayz = $this->get('jms_serializer');
         $content = $this->getRequest()->getContent();
 
@@ -174,8 +194,7 @@ class ProductController extends Controller
         foreach ($products['products'] as $product) {
 
             $product = $serialayz->fromArray($product, 'CSVParceBundle\Entity\Product');
-            dump($product);
-            die();
+
             /**@var Product $prod */
             $prod = $em->getRepository('CSVParceBundle:Product')->find($product->getId());
 
@@ -196,30 +215,20 @@ class ProductController extends Controller
 
         $response = new Response();
 
-        return $response->setContent('ok');
+        return $response->setContent('OK');
     }
 
     private function processForm(Product $product)
     {
-        $serialayz = $this->get('jms_serializer');
-
-
 
         $statusCode = $product->getId() ? 204 : 201;
 
-
         $form = $this->createForm(new ProductType(), $product);
 
-        $product = $serialayz->deserialize($this->getRequest()->getContent(), 'array', 'json');
-
-        $product = $serialayz->fromArray($product['product'], 'CSVParceBundle\Entity\Product');
-
-        $form->bind($product);
-
+        $form->bind($this->getRequest());
 
         $validator = $this->get('validator');
         $errors = $validator->validate($product);
-
 
         if ($product->getDiscontinued()) {
             $product->setDiscontinuedDate(new \DateTime('now'));
@@ -241,9 +250,8 @@ class ProductController extends Controller
             return $response;
         }
 
-
         return RView::create($form, 400);
     }
 
-
 }
+
